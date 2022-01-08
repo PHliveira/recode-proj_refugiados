55ad2b1e517d5fdca1f4176a859586a528054bd8
PHliveira <96453416+PHliveira@users.noreply.github.com>
2022-01-08 02:29:28 -0300

    Add files via upload
    controller - pagina - saude

---
+++ Refugiados/Refugiados/Controllers/OdontoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Refugiados.Controllers
+{
+    public class OdontoController : Controller
+    {
+        public IActionResult Odonto()
+        {
+            return View();
+        }
+    }
+}
